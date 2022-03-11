21a5522457abb0ec116dd8479c40511b95d26941
Abdulganiyu Abubakar Sidiq <85228324+Ayoola32@users.noreply.github.com>
2022-03-12 00:24:58 +0100

    6print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print single digit numbers
- *
- * Description: print single digit numbers
- *  
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int n;
 
- int main(void)
- {
-	 int i;
-
-	 i = 48;
-
-	 while (i < 58)
-	 {
-		 putchar(i);
-		 i++;
-	 }
-
-	 putchar(10);
-
-	 return (0);
- }
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
