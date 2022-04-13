1d918dae4afa3ecea6ed26fc14cd4b6913e449df
Abdulganiyu Abubakar Sidiq <85228324+Ayoola32@users.noreply.github.com>
2022-04-13 17:49:43 +0100

    3 cal

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,33 +1,25 @@
-#include "3-calc.h"
+#ifndef _CALC_H_
+#define _CALC_H_
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
-  * get_op_func - choosing the right func for the operator
-  * @s: the operator
-  *
-  * Return: result of function or error
-  */
-int (*get_op_func(char *s))(int, int)
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
 {
-	/* declarations */
-	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}
-	};
-	int i;
-	/* matches operator to function */
-	i = 0;
-	while (ops[i].op)
-	{
-		if (ops[i].op[0] == *s && s[1] == '\0')
-			return (ops[i].f);
-		i++;
-	}
-	/* bad operator, print error */
-	printf("Error\n");
-	exit(99);
-}
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
